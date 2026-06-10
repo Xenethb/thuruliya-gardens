e631e4a8f4f7fce42e88b7b7c9363d2c03f88dc8
seneth Boteju - s23010602 <senethboteju@gmail.com>
2026-06-10 08:34:54 +0530

    Fix categories page module export error

---
+++ src/app/(shop)/categories/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function CategoriesPage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <h1 className="text-2xl font-serif text-[#2C3E2B]">Categories Page</h1>
+            {/* You can build out this page later! */}
+        </div>
+    );
+}
